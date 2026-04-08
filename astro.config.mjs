6c32cc644bbae0514479e79814b1c7df9b68d443
Hazarral <huynhquochuy2006@gmail.com>
2026-04-08 14:05:13 +0700

    Yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hazarral.github.io',
+  base: '/',
+  
   vite: {
     plugins: [tailwindcss()],
   },
